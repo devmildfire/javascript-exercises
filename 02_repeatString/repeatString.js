6fb59ec8c55735a79a6252fd214d8b91aeb85985
devmildfire <mildfire@gmail.com>
2022-03-21 20:01:06 +0500

    complete exercise 02 - repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return `ERROR`; //returns ERROR with negative numbers
+    } else {
+        let resultstring = ``; //by default the result is a null string
+        for (i = 1; i <= number; i++) {
+            resultstring += string; //result is an original string 
+                                    //reteated number times
+        }
+        return resultstring;
+    }
 };
 
 // Do not edit below this line

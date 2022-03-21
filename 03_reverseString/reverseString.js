fa1e08194a43da1ebc090c3610b6636c831350a0
devmildfire <mildfire@gmail.com>
2022-03-21 20:22:19 +0500

    complete exercise 03 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let charArray = []; // an empty array to hold characters of the input string
+                        // as its items for reversal
+    for (i = 0; i < inputString.length; i++) {
+        charArray[i] = inputString.charAt(i); // fill array with characters of
+                                              // the input string
+    }
+    charArray.reverse();
+    let outputString = charArray.join(``); //produce a string out of array
+                                           // without separators
+    return outputString;
 };
 
 // Do not edit below this line

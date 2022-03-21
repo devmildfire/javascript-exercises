e2d1d66796681acf13ab2868552ca37bc2c6e187
devmildfire <mildfire@gmail.com>
2022-03-21 23:54:27 +0500

    complete exercise 04 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(initialArray, ...restArgs) {
+    
+    for (const itemToRemove of restArgs) {  // a for loop for all the arguments
+                                             // except the initialArray
 
+        for (i = 0; i < initialArray.length; i++) { //a loop for initialArray items
+            if (initialArray[i] === itemToRemove) {
+               initialArray[i]  = null; //set to null items of initialArray
+            }                           // which are equal to ones
+                                        // we need to remove                         
+        }                                     
+    }
+    let outputArray = initialArray.filter(element => {  //filter nulls from
+                                                        //initialArray
+        return element !== null;
+    });
+    return outputArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

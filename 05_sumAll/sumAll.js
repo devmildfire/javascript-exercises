efb17d87c34ab5cdf129a0b3260f8711f50b0979
devmildfire <mildfire@gmail.com>
2022-03-22 10:19:31 +0500

    complete exercise 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const isValidArgument = function(argument) {
+    if (typeof argument === `number` &&             // if `argument` is a number
+       (Math.floor(Math.abs(argument)) === argument) ) { // and a positive integer
+        return true;                                // it is valid
+    } else {
+        return false; 
+    }
+}
+
+
+
+const sumAll = function(firstNumber, secondNumber) {
+
+    if ( !isValidArgument(firstNumber) ||       // if either of arguments is
+         !isValidArgument(secondNumber) ) {     // not valid
+        return `ERROR`;                         // then it is an ERROR output
+    } else {
+        return summ = (  Math.abs(firstNumber - secondNumber) + 1  ) *  
+               (firstNumber + secondNumber)/2;  // S = (n1 + n2)*N/2
+    }
 
 };
 

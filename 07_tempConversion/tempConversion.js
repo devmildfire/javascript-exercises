e9043d0f05e42c2c0b4c1ed7c454d3485e23a1a3
devmildfire <mildfire@gmail.com>
2022-03-22 14:27:57 +0500

    complete exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = (temp - 32)*5/9;
+  if (celsius % 1 == 0) {
+    return celsius;
+  } else {
+    return Number(celsius.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = (temp*9/5) + 32;
+  if (fahrenheit % 1 == 0) {
+    return fahrenheit;
+  } else {
+    return Number(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line

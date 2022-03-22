813ca3cc4286b8302a116d07c89d523b7dc21022
devmildfire <mildfire@gmail.com>
2022-03-22 17:27:01 +0500

    complete exercise 09 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const filterString = function(inputString) {
+    let filteredString = inputString.replace(/[^\w]/gi, '');
+    return filteredString;
+}
+
+function reverseString(inputString) {
+    let outputString =  inputString.split(``).reverse().join(``);
+    return outputString;
+}
+
+const palindromes = function (inputString) {
+
+    inputString = inputString.toLowerCase();
+
+    let filteredString = filterString(inputString);
+
+    let compareString = filteredString;
+
+    let reversedString = reverseString(filteredString);
+
+    return reversedString === compareString ? true : false;
 
 };
 

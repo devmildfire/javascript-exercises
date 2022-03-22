4dca97551326f3ae2e0826cf9aac66abd47333b4
devmildfire <mildfire@gmail.com>
2022-03-22 15:04:46 +0500

    complete exercise 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(number_one, number_two) {
+	return result = number_one + number_two;
 };
 
-const subtract = function() {
-	
+const subtract = function(number_one, number_two) {
+  return result = number_one - number_two;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	let result = 0;
+  for (i = 0; i < inputArray.length; i++) {
+    result += inputArray[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(inputArray) {
+  let result = inputArray[0];
+  for (i = 1; i < inputArray.length; i++) {
+    result *= inputArray[i];
+  }
+  return result;
+};
+
+const power = function(number, power) {
+	if (power === 0) {
+    return 1;
+  } else {
+    let result = number;
+    for (i = 1; i < power; i++) {
+      result *= number;
+    }
+    return result;
+  }
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 1 || number === 0) {
+    return 1;   // 0! and 1! are both equal to 1
+  }
+  let result = number;
+  result *= factorial(number - 1);
+  return result;
 };
 
 // Do not edit below this line
